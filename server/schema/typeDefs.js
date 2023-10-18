e03773877e7b943fa796b9e158dd8e5faac154f4
Ben Cooper <39800139+BenACooper@users.noreply.github.com>
2023-10-18 18:05:38 -0400

    added typeDefs

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -15,4 +15,20 @@ type Book {
     image: String
     link: String
 }
-`
+
+type Auth {
+    token: ID!
+    user: User
+  }
+
+type Query {
+    users: [users]
+}
+
+type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(username: String, email: String, password: String!): Auth
+    addBook(userId: ID!, book: BookInput): User
+    removeBook(userId: ID!, bookId: ID!): User
+    }
+`;
